a4cb08ab6eede0d5bff575e75e6bbe3542647bdc
JoseRobertoGDeveloper <joserobertoq@outlook.com>
2025-03-14 15:15:20 -0600

    Mejorar pruebas de Playwright - se actualiza el primer test para usar el contexto del navegador

--- tests/UBasictest.spec.js
+++ tests/UBasictest.spec.js
@@ -1,7 +1,11 @@
 import { test } from '@playwright/test';
 
-test('First Playwright test', async ({ page }) => {
-    //const context = await browser.newContext();
-    //const page = await context.newPage();
+test('First Playwright test', async ({ browser }) => {
+    const context = await browser.newContext();
+    const page = await context.newPage();
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+});
+
+test('Page Playwright test', async ({ page }) => {
+    await page.goto("https://google.com");
 });
