81ea0e904e3c6f2cfbe604c1b5776b14cc272e82
JoseRobertoGDeveloper <joserobertoq@outlook.com>
2025-03-26 19:04:14 -0600

    Pruebas de calendar

--- tests/Calendar.spec.js
+++ tests/Calendar.spec.js
@@ -6,6 +6,8 @@ test('Calendar validations', async ({ page }) =>
     const date = "15";
     const year = "2027";
 
+    const expectedList = [monthNumber, date, year];
+
     await page.goto('https://rahulshettyacademy.com/seleniumPractise/#/offers');
 
     await page.locator('.react-date-picker__inputGroup').click();
@@ -19,4 +21,12 @@ test('Calendar validations', async ({ page }) =>
     await page.locator('.react-calendar__year-view__months__month').nth(monthNumber - 1).click();
 
     await page.locator("//abbr[text()='"+date+"']").click();
+
+    let inputs = await page.locator("input.react-date-picker__inputGroup__input").all();
+
+    for (let i = 0; i < inputs.length(); i++) {
+        const value = inputs[i].getAttribute('value');
+        expect(value).ToEqual(expectedList[i]);
+        console.log(value);
+    }
 });
