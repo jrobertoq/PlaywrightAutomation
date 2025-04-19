24a09421ecde723cf204f22363829730e9cdcded
JoseRobertoGDeveloper <joserobertoq@outlook.com>
2025-04-18 19:10:41 -0600

    Refactor pruebas de API: crear clase APIUtils para manejar autenticación y creación de pedidos

--- tests/WebAPIPart1.spec.js
+++ tests/WebAPIPart1.spec.js
@@ -1,46 +1,21 @@
 import { expect, test, request } from '@playwright/test';
+import { APIUtils } from './utils/APIUtils.js';
 
 const loginPayload = {userEmail: "joserobertoq@outlook.com", userPassword: "2Hidroxil@"}
 const orderPayload = {orders: [{country: "India", productOrderedId: "67a8dde5c0d3e6622a297cc8"}]}
-let token;
-let orderId;
+let response;
 
 test.beforeAll( async() => {
-    // Login API
     const apiContext = await request.newContext();
-    const loginResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login", 
-        {
-            data: loginPayload
-        })
-    
-    expect(loginResponse.ok()).toBeTruthy();
-
-    const responseBody = await loginResponse.json();
-    token = responseBody.token;
-    console.log(token);
-
-    // Creare an order
-    const orderResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order",
-        {
-            data: orderPayload,
-            headers: {
-                'Authorization': token,
-                'Content-Type': 'application/json'
-            }
-        }
-    );
-
-    const orderResponseJson = await orderResponse.json();
-    console.log(orderResponseJson);
-    orderId = orderResponseJson.orders[0];
-    console.log(orderId);
+    const apiUtils = new APIUtils(apiContext, loginPayload);
+    response = await apiUtils.createOrder(orderPayload);
 });
 
 
-test('Client App Login', async ({ page }) => {
+test('Place the order', async ({ page }) => {
     page.addInitScript((value) => {
         window.localStorage.setItem('token', value);
-    }, token);
+    }, response.token);
 
     await page.goto("https://rahulshettyacademy.com/client");
 
@@ -52,7 +27,7 @@ test('Client App Login', async ({ page }) => {
 
     for (let i = 0; i < await rows.count(); i++) {
         const rowOrderId = await rows.nth(i).locator("th").textContent();
-        if (orderId.includes(rowOrderId)) {
+        if (response.orderId.includes(rowOrderId)) {
             await rows.nth(i).locator("button").first().click();
             break;
         }
@@ -62,5 +37,5 @@ test('Client App Login', async ({ page }) => {
 
     await page.pause();
 
-    expect(orderId.includes(orderIdDetailsPage)).toBeTruthy();
+    expect(response.orderId.includes(orderIdDetailsPage)).toBeTruthy();
 });
