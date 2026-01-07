79e07f828d1432002d97e4c64fa08647ffc624b3
JoseRobertoGDeveloper <joserobertoq@outlook.com>
2026-01-07 05:01:18 -0600

    Aumentar el tiempo de espera de las pruebas a 100 segundos en playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -14,7 +14,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
-  timeout: 30 * 1000,
+  timeout: 100 * 1000,
   expect: {
     timeout: 5000,
   },
